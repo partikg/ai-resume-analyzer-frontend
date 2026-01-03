dd13fc02e00fb7a3055c121aad10bb532ff2dad2
Pratik Raut <pratik@example.com>
2026-01-03 22:37:35 +0530

    Updated

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,89 @@
+'use client'
+
+import { useState } from 'react'
+import {
+    Dialog,
+    DialogPanel,
+    PopoverGroup,
+} from '@headlessui/react'
+import {
+    Bars3Icon,
+    XMarkIcon,
+} from '@heroicons/react/24/outline'
+import Link from 'next/link'
+
+export default function Header() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    return (
+        <header className="bg-gray-900">
+            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+                <div className="flex lg:flex-1">
+
+                    <Link href="/" className="-m-1.5 p-1.5">
+                        <img
+                            alt=""
+                            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                            className="h-8 w-auto"
+                        />
+                    </Link>
+                </div>
+
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        onClick={() => setMobileMenuOpen(true)}
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon aria-hidden="true" className="size-6" />
+                    </button>
+                </div>
+
+                <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+
+
+                </PopoverGroup>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+
+                </div>
+            </nav>
+
+            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                <div className="fixed inset-0 z-50" />
+                <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
+                    <div className="flex items-center justify-between">
+
+                        <Link href="/" className="-m-1.5 p-1.5">
+                            <img
+                                alt=""
+                                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                                className="h-8 w-auto"
+                            />
+                        </Link>
+                        <button
+                            type="button"
+                            onClick={() => setMobileMenuOpen(false)}
+                            className="-m-2.5 rounded-md p-2.5 text-gray-400"
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon aria-hidden="true" className="size-6" />
+                        </button>
+                    </div>
+
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-white/10">
+                            <div className="space-y-2 py-6">
+
+
+                            </div>
+                            <div className="py-6">
+
+                            </div>
+                        </div>
+                    </div>
+                </DialogPanel>
+            </Dialog>
+        </header>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,214 @@
-import Image from "next/image";
+'use client'
+import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [resume, setResume] = useState(null)
+  const [jd, setJd] = useState("")
+  const [result, setResult] = useState(null)
+  const [pdfUrl, setPdfUrl] = useState(null);
+  const [company, setCompany] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [loading, setLoading] = useState(false)
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    setLoading(true)
+
+    const formData = new FormData()
+    formData.append("resume", resume)
+    formData.append("jd", jd)
+    formData.append("company", company)
+    formData.append("jobTitle", jobTitle)
+
+    axios.post('http://localhost:3000/api/resume/upload', formData)
+      .then((res) => {
+        setResult(res.data)
+        setPdfUrl(res.data.pdfUrl);
+        setLoading(false)
+        console.log(res.data)
+      })
+      .catch((error) => {
+        setLoading(false)
+        console.log(error)
+      })
+
+  }
+
+  const getLabel = (score) => {
+    if (score >= 75) {
+      return "Good";
+    }
+    else if (score >= 50) {
+      return "Average";
+    }
+    else {
+      return "Poor"
+    }
+  }
+
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <div className="flex-1 justify-items-center">
+
+        <div className="w-full max-w-4xl border-2">
+
+          <form onSubmit={submitHandler} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="companyname">
+                Company Name
+              </label>
+              <input
+                className="border-2"
+                placeholder="give company name"
+                onChange={(e) => setCompany(e.target.value)}
+              ></input>
+            </div>
+
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="jobtitle">
+                Job Title
+              </label>
+              <input
+                className="border-2"
+                placeholder="give job title"
+                onChange={(e) => setJobTitle(e.target.value)}
+              ></input>
+            </div>
+
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="jd">
+                Job Description
+              </label>
+              <textarea
+                className="border-2"
+                placeholder="Paste job description"
+                onChange={(e) => setJd(e.target.value)}
+              ></textarea>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="resume">
+                Upload resume file
+              </label>
+              <input
+                className="border-2"
+                type="file"
+                onChange={(e) => setResume(e.target.files[0])}
+              />
+            </div>
+
+            <div className="flex items-center justify-between">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" disabled={loading}>
+                {loading ? "Analyzing..." : "Analyze"}
+              </button>
+            </div>
+          </form>
+
+
+          {/* result */}
+          {result && (
+
+            // left side
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+
+              <div className="border rounded-lg p-3 bg-white">
+                <iframe
+                  src={pdfUrl}
+                  className="w-full h-[500px] border"
+                />
+              </div>
+
+              {/* right side */}
+              <div className="rounded-lg p-6">
+
+                <h2 className="font-extrabold text-3xl mb-4">Resume Analysis</h2>
+
+                <div className="text-center mb-6">
+                  <p className="text-gray-500">Overall Resume Score</p>
+                  <p className="text-5xl font-bold">
+                    {result.overallScore}%
+                  </p>
+                </div>
+
+                <div className="space-y-2">
+                  <p><b>Tone and Style:</b> {result.toneScore}/100 ({getLabel(result.toneScore)})</p>
+
+                  <p><b>content:</b> {result.contentScore}/100 ({getLabel(result.contentScore)})</p>
+
+                  <p><b>structure:</b> {result.structureScore}/100 ({getLabel(result.structureScore)})</p>
+
+                  <p><b>skills:</b> {result.skillsScore}/100 ({getLabel(result.skillsScore)})</p>
+                </div>
+
+                <div className="mt-3 mb-4 p-2 border">
+                  <span className="font-semibold">ATS Score: </span>
+                  <span className="text-xl font-bold">
+                    {result.atsScore}%
+                  </span>
+                </div>
+
+                <h6 className="font-semibold">Needs Improvement:</h6>
+                <ul className="list-disc text-sm pl-5 text-gray-700">
+                  {result.improvements.map(imp => (
+                    <li key={imp}>{imp}</li>
+                  ))}
+                </ul>
+
+
+                {/* resume details */}
+                <div className="mt-6 text-sm">
+
+                  <div>
+                    <p className="mt-2"><b>Matched Skills:</b></p>
+                    <ul>
+                      {result.matchedSkills.map(skill => (
+                        <li key={skill}>{skill}</li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  <div>
+                    <p className="mt-2"><b>Missing Skills:</b></p>
+                    <ul>
+                      {result.missingSkills.map(skill => (
+                        <li key={skill}>{skill}</li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  <div>
+                    <p className="mt-2"><b>Job Required Skills:</b></p>
+                    <ul>
+                      {result.jobskills.map(skill => (
+                        <li key={skill}> {skill}</li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  <div>
+                    <p className="mt-2"><b>Skills Found in Resume:</b></p>
+                    <ul>
+                      {result.resumeskills.map(skill => (
+                        <li key={skill}> {skill}</li>
+                      ))}
+                    </ul>
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+          )}
+
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+
+      </div>
+    </>
   );
 }
